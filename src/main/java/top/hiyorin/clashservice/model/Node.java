24aaf594defd9912641320dae1ffbb11febf77a6
桜与白 <318099184@qq.com>
2024-05-17 01:43:15 +0800

    ✨ 使用新的实体类以支持复数个节点信息

---
+++ src/main/java/top/hiyorin/clashservice/model/Node.java
@@ -0,0 +1,31 @@
+package top.hiyorin.clashservice.model;
+
+public class Node {
+    private Integer id;
+    private String cache;
+    private String uuid;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCache() {
+        return cache;
+    }
+
+    public void setCache(String cache) {
+        this.cache = cache;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+}
