6d762d6e1861754cac4711603a9dc505ed51404b
桜与白 <318099184@qq.com>
2024-05-18 08:10:41 +0800

    💥 对数据库结构做出不兼容更改

--- src/main/java/top/hiyorin/clashservice/mapper/ClashMapper.java
+++ src/main/java/top/hiyorin/clashservice/mapper/ClashMapper.java
@@ -1,15 +1,16 @@
 package top.hiyorin.clashservice.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import top.hiyorin.clashservice.model.Node;
 import top.hiyorin.clashservice.model.User;
 
 @Mapper
 public interface ClashMapper {
     User selectUser(String base64);
 
-    String getCache();
+    Node selectNode(Integer id);
 
-    String getSubscribeUrl();
+    String getSubscribeUrl(Integer id);
 
-    Integer updateCache(String cache);
+    Integer updateCache(String cache, Integer id);
 }
